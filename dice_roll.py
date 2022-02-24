2ecb2e69f352c997e2488eeab3abde5c810f1134
Serbanescu <alexandru.serbanescu@vodafone.com>
2022-02-24 14:51:43 +0200

    test

---
+++ dice_roll.py
@@ -0,0 +1,14 @@
+import random
+roll = random.randint(1,6)
+print("**************************************************************************")
+print("> D I C E   v0.01 <")
+print("**************************************************************************")
+print("This is a guessing game based on a dice.")
+print("The PC rolls a dice and you have to guess what number the computer rolled.")
+print("Good luck!")
+print("**************************************************************************")
+guess = int(input("Guess the dice roll:"))
+if guess == roll:
+    print("Correct the PC rolled a " + str(roll))
+else:
+    print("Wrong! The PC rolled a " + str(roll))
---
+++ tax.py
@@ -0,0 +1,4 @@
+amount = int(input("What is the amount? \n"))
+tax = float(input("What is the tax? \n"))
+total = amount + amount * tax
+print("Total = " + str(total))
