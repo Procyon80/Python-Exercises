d62faeb09fe1281a568e8baaada703f97342dd9e
Procyon80 <aserbanescu@gmail.com>
2022-04-12 12:16:08 +0300

    few changes

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-print("Hello world!")
-name = input("What is your name? \n")
-print("Hello " + name)
+#print("Hello world!")
+#name = input("What is your name? \n")
+#print("Hello " + name)
 #this is just a test
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+ys = 200 + np.random.randn(100)
+x = [x for x in range(len(ys))]
+
+plt.plot(x, ys, '-')
+plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+
+plt.title("Sample Visualization")
+plt.show()
--- test_spark.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession \
-    .builder \
-    .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
-    .getOrCreate()
