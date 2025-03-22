9700662d6dbf9a266cc9b0849ab5d2932c211d5d
Procyon80 <aserbanescu@gmail.com>
2025-03-22 14:29:11 +0200

    refactor dice roll logic and add input validation

--- dice_roll.py
+++ dice_roll.py
@@ -1,15 +1,18 @@
 
 import random
-roll = random.randint(1,6)
+dice_roll = random.randint(1,6)
 print("**************************************************************************")
-print("> D I C E   v0.01 <")
+print("> D I C E   v0.02 <")
 print("**************************************************************************")
 print("This is a guessing game based on a dice.")
 print("The PC rolls a dice and you have to guess what number the computer rolled.")
 print("Good luck!")
 print("**************************************************************************")
-guess = int(input("Guess the dice roll:"))
-if guess == roll:
-    print("Correct the PC rolled a " + str(roll))
-else:
-    print("Wrong! The PC rolled a " + str(roll))
+try:
+    guess = int(input("Guess the dice roll:"))
+    if guess == dice_roll:
+        print(f"Correct the PC rolled a {dice_roll}")
+    else:
+        print(f"Wrong! The PC rolled a {dice_roll}")
+except ValueError:
+    print("Invalid input! Please enter an integer.")
