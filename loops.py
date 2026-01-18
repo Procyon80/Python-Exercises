c9817592cf131bcf3322ea8e06e12582fd807ef2
Procyon80 <aserbanescu@gmail.com>
2026-01-18 10:25:37 +0200

    feat: add loops.py with seat availability logic

---
+++ loops.py
@@ -0,0 +1,11 @@
+seats = 10
+while seats > 0:
+    print(f"{seats} seats available.")
+    seats = seats - 1
+print("No seats available.")
+
+i = 0
+while i < 10:
+    print(f"{i} SEATS AVAILABLE.")
+    i = i + 1
+print("NO SEATS AVAILABLE.")
